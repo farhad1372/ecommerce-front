81b4bdc668b8a2479027ba905e1158bda869feaa
farhad <farhadfallahpour.eleceng@gmail.com>
2025-06-02 21:07:47 +0330

    style(header): make header responsive with Tailwind

---
+++ components/header/index.tsx
@@ -0,0 +1,91 @@
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+// import SidebarPanel from "../sidebar";
+
+const Header = () => {
+  return (
+    <>
+      <header className="border-b py-3 md:py-4 lg:py-5">
+        <div className="container mx-auto">
+          <div className="flex items-center gap-10 ">
+            <div className="hidden md:block">
+              <h3 className="font-bold text-xl text-primary">
+                E-Commerce Product
+              </h3>
+            </div>
+            <NavigationMenu viewport={true} className="z-50">
+              <NavigationMenuList className="w-80 md:w-auto text-left flex justify-start">
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger>Products</NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <ul className="grid gap-2 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                      <li className="row-span-3">
+                        <NavigationMenuLink asChild>
+                          <a
+                            className="from-muted/50 to-muted flex h-full w-full flex-col justify-end rounded-md bg-linear-to-b p-6 no-underline outline-hidden select-none focus:shadow-md"
+                            href="/asd"
+                          >
+                            <div className="mt-4 mb-2 text-lg font-medium">
+                              shadcn/ui
+                            </div>
+                            <p className="text-muted-foreground text-sm leading-tight">
+                              Beautifully designed components built with
+                              Tailwind CSS.
+                            </p>
+                          </a>
+                        </NavigationMenuLink>
+                      </li>
+                      <ListItem href="/docs" title="Introduction">
+                        Re-usable components built using Radix UI and Tailwind
+                        CSS.
+                      </ListItem>
+                      <ListItem href="/docs/installation" title="Installation">
+                        How to install dependencies and structure your app.
+                      </ListItem>
+                      <ListItem
+                        href="/docs/primitives/typography"
+                        title="Typography"
+                      >
+                        Styles for headings, paragraphs, lists...etc
+                      </ListItem>
+                    </ul>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+          </div>
+        </div>
+      </header>
+      {/* <SidebarPanel /> */}
+    </>
+  );
+};
+
+function ListItem({
+  title,
+  children,
+  href,
+  ...props
+}: React.ComponentPropsWithoutRef<"li"> & { href: string }) {
+  return (
+    <li {...props}>
+      <NavigationMenuLink asChild>
+        <Link href={href}>
+          <div className="text-sm leading-none font-medium">{title}</div>
+          <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">
+            {children}
+          </p>
+        </Link>
+      </NavigationMenuLink>
+    </li>
+  );
+}
+
+export default Header;
