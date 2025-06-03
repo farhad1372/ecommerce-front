e21fa078ea93ed5a567d09c26b53c26e67dd60fe
farhad <farhadfallahpour.eleceng@gmail.com>
2025-06-03 22:58:30 +0330

    feat(ui): add sidebar component and its hook

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,5 +1,55 @@
-const Sidebar = () => {
-  return <>Sidebar</>;
+import { Grid, Home } from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "../ui/sidebar";
+
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+    {
+    title: "Product",
+    url: "#",
+    icon: Grid,
+  },
+
+];
+
+const SidebarPanel = () => {
+  return (
+    <>
+      <Sidebar>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupLabel>Application</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+    </>
+  );
 };
 
-export default Sidebar;
+export default SidebarPanel;
