1692f2a1dbe2a5163896ebd79f3b0a16d185363a
farhad <farhadfallahpour.eleceng@gmail.com>
2025-06-04 08:11:52 +0330

    feat(types): move product types to types/product.ts for reusability

---
+++ types/product.ts
@@ -0,0 +1,26 @@
+  export interface Product {
+    id: number;
+    name: string;
+    created_at: string;
+    price: number;
+    description: string;
+    slug: string;
+    main_image: string | null;
+    images: string[] | null;
+    rate_count: number;
+    rate_value: number;
+    attributes: Attribute[];
+    product_attributes: ProductAttribute[];
+  }
+
+  export interface Attribute {
+    id: number;
+    type: 'color' | 'size';
+    value: string;
+  }
+
+  export interface ProductAttribute {
+    id: number;
+    price: number;
+    attribute_ids: number[];
+  }
